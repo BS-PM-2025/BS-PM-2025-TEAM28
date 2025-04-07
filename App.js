1341886e05e7a99bd4563820ce6de2979a13ec5b
Yarin65 <yarinev@ac.sce.ac.il>
2025-04-07 18:26:11 +0300

    123

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Yarin the Goat</Text>
+      <Text style={styles.text}>Yarin</Text>
     </View>
   );
 }
